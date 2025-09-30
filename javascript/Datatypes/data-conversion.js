effb68a04997a9d09cdaa5995849f25b8f924ec8
priyanshu_pal9351 <palpriyanshu9351@gmail.com>
2025-09-30 10:26:19 +0000

    Conversions

---
+++ javascript/Datatypes/data-conversion.js
@@ -0,0 +1,36 @@
+
+let score = false
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// look how the conversion happens
+// "33" => 33
+// "33abc" => NaN(not a number) --> type --> number
+// true => 1 / false => 0
+
+ 
+let isLoggedIn = 1
+
+// console.log(typeof isLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(typeof booleanIsLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
+// we did conversion of number to string , number to boolean , and string to number by using Number(), String(), and Boolean()
+
